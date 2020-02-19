84d26c4f476d31a97aa3b6ecf0f2eb05488fae29
RiskTolerance <isaac.druin@gmail.com>
2020-02-18 18:13:00 -0800

    css done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import HomeCards from "./components/HomeCards";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <HomeCards />
       </div>
     );
   }
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import axios from 'axios'
+
+// class App extends Component {
+
+//   state = {
+//     beers: [], //fill this array of beers with beers from the api
+//     dogs : []
+//   }
+
+//   async componentDidMount(){
+//     //console.log('happens once on mount')
+
+//     //.then promise
+//     axios.get(`https://ih-beers-api2.herokuapp.com/beers/`).then(res => { //This takes some time by the time it gets back
+//         this.setState({
+//           beers:res.data
+//         })
+//     })
+
+//     //async await promise
+//     let response = await axios.get(`https://dog.ceo/api/breed/boxer/images/random/30`)
+//     this.setState({
+//         dogs:response.data.message
+//     })
+
+//   }
+
+//   showTheBeers = () => {
+//     return this.state.beers.map(eachBeer => {
+//       return <li>{eachBeer.name}</li>
+//     })
+//   }
+
+//   showTheDogs = () => {
+//     console.log(this.state.dogs)
+//     return this.state.dogs.map(eachDog => {
+//       return <img src={eachDog} />
+//     })
+//   }
+
+//   render() { //Never set state in here
+//     //console.log('render may happen more than once', this)
+
+//     return (
+//       <div>
+//           Axios with React!
+//           {this.showTheDogs()}
+//           {this.showTheBeers()}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
